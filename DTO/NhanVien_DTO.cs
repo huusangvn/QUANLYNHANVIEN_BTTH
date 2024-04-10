905dc4e7857e8b0693d1fcaffcc124047e629a91
huusangnv@gmail.com <huusangnv@gmail.com>
2024-04-10 10:09:58 +0700

    Đã làm xong phần tìm kiếm cùng với click vào datagridview hiển thị lên thông tin tương ứng..

--- DTO/NhanVien_DTO.cs
+++ DTO/NhanVien_DTO.cs
@@ -13,12 +13,15 @@ namespace DTO
         private string tennv;
         private string phai;
         private DateTime ngaysinh;
-        private string tencv;
         public string Manv { get => manv; set => manv = value; }
         public string Holot { get => holot; set => holot = value; }
         public string Tennv { get => tennv; set => tennv = value; }
         public string Phai { get => phai; set => phai = value; }
         public DateTime Ngaysinh { get => ngaysinh; set => ngaysinh = value; }
+        public string Macv { get => macv; set => macv = value; }
         public string Tencv { get => tencv; set => tencv = value; }
+
+        private string macv;
+        private string tencv;
     }
 }
